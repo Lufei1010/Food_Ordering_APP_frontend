cf6d4b252fe3b7b781bf90278c31e90a1f584c61
Lufei1010 <lufeiwu1010@gmail.com>
2024-11-09 22:30:43 +0200

    Build Search results_Filter on search results page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   searchQuery: z.string({
@@ -19,12 +20,20 @@ type Props = {
   onSubmit: (formData: SearchForm) => void;
   placeholder: string;
   onReset?: () => void;
+  searchQuery: string;
 };
 
-const SearchBar = ({ onSubmit, onReset, placeholder }: Props) => {
+const SearchBar = ({ onSubmit, onReset, placeholder, searchQuery }: Props) => {
   const form = useForm<SearchForm>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      searchQuery,
+    },
   });
+
+  useEffect(() => {
+    form.reset({ searchQuery })
+  },[form, searchQuery]);
 
   const handleReset = () => {
     form.reset({
@@ -40,7 +49,7 @@ const SearchBar = ({ onSubmit, onReset, placeholder }: Props) => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className={`flex items-center flex-1 gap-3 justify-between fle-row border-2 rounded-full p-3 mx-5 ${
+        className={`flex items-center flex-1 gap-3 justify-between fle-row border-2 rounded-full p-3 ${
           form.formState.errors.searchQuery && "border-red-500"
         }`}
       >
@@ -64,16 +73,14 @@ const SearchBar = ({ onSubmit, onReset, placeholder }: Props) => {
             </FormItem>
           )}
         />
-        {form.formState.isDirty && (
           <Button
             onClick={handleReset}
             type="button"
             variant="outline"
             className="rounded-full"
           >
-            Clear
+            Reset
           </Button>
-        )}
         <Button type="submit" className="rounded-full bg-orange-500">
           Search
         </Button>
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,33 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
+export type SearchState = {
+  searchQuery: string;
+};
 const SearchPage = () => {
   const { city } = useParams();
-  const { results, isLoading} = useSearchRestaurants(city);
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "", // default empty when the page loads at the first time
+  });
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery, // update the searchQuery with the new search value
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+    }));
+  };
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -16,9 +38,15 @@ const SearchPage = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gapt-5">
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gapt-5 p-4 sm:p-6">
       <div id="cuisines-list">insert cuisines here: )</div>
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeholder="Search by Cuisine or Restaurant Name"
+          onReset={resetSearch}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((restaurant) => (
           <SearchResultCard restaurant={restaurant} />
@@ -27,17 +55,18 @@ const SearchPage = () => {
     </div>
   );
 
-
   return (
-    <span>User searched for {city}{" "}
     <span>
-      {results?.data.map((restaurant) => (
-        <span>
-          found - {restaurant.restaurantName}, {restaurant.city}
-        </span>
-      ))}
-      </span></span>
-  )
-}
+      User searched for {city}{" "}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
+};
 
 export default SearchPage;
