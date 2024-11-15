e692e9478157cb5ba431092e963e63edf3f83c16
Lufei1010 <lufeiwu1010@gmail.com>
2024-11-15 15:29:00 +0200

    Implement Restaurant Detail Page: Display menu items, adding items to the cart, and sum up total cost

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,11 +1,55 @@
 import { useGetMyRestaurants } from "@/api/RestaurantApi";
+import MenuItem from "@/components/MenuItem";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { MenuItem as MenuItemType } from "../types";
 
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetMyRestaurants(restaurantId);
+
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  //1. Check if the item is already in the cart;
+  //2. if the item is in cart, update the quantity;
+  // 3. if item is not in cart, add it as a new item
+  const addToCart = (menuItem: MenuItemType) => {
+    setCartItems((prevCartItems) => {
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+
+      let updatedCartItems;
+
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+      return updatedCartItems;
+    });
+  };
 
   if (isLoading || !restaurant) {
     return "Loading...";
@@ -21,7 +65,20 @@ const DetailPage = () => {
       </AspectRatio>
       <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
         <div className="flex flex-col gap-4">
-            <RestaurantInfo restaurant={restaurant} />
+          <RestaurantInfo restaurant={restaurant} />
+          <span className="text-2xl font-bold tracking-tight">Menu</span>
+          {restaurant.menuItems.map((menuItem) => (
+            <MenuItem
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
+          ))}
+        </div>
+
+        <div>
+          <Card>
+            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+          </Card>
         </div>
       </div>
     </div>
