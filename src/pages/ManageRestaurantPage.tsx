1629d605375142384d063b782a24c7050beafb57
Lufei1010 <lufeiwu1010@gmail.com>
2024-11-21 15:00:17 +0200

    Manage Restaruant Page_Orders' display order info  and options of status.

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,20 +1,52 @@
-import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant, useGetMyRestaurantOrders, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
+import { TabsTrigger } from "@radix-ui/react-tabs";
 
 const ManageRestaurantPage = () => {
-  const {createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
-  const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
-
-  const isEditing  = !!restaurant;
+  const { orders } = useGetMyRestaurantOrders();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
+  const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList className="flex gap-x-6">
+        <TabsTrigger
+          value="orders"
+          className="hover:bg-white hover:text-gray-900 transition-colors duration-200 rounded-lg p-2"
+        >
+          Orders
+        </TabsTrigger>
+        <TabsTrigger
+          value="manage-restaurant"
+          className="hover:bg-white hover:text-gray-900 transition-colors duration-200 rounded-lg p-2"
+        >
+          Manage Restaurant
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
-}
+};
 
 export default ManageRestaurantPage;
